a46ead749e9105ef727ec8f45d56de5ac6e16ac3
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-03-02 19:56:06 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,44 @@
+print("Hello, World!")
+if 5 > 2:
+  print("Five is greater than two!")
+  #I'm enjoying this.
+  print("I love Kenya!")
+  x = 5 
+  y = "Me"
+  print(type(x))
+  print(type(y))
+  z = "Wuanita"
+  print(z)
+  #double quotes are similar to single quotes
+  z = 'Wuanita'
+  print(z)
+  my_name = "Akk"
+  myname = "Akk"
+  _my_name = "Akk"
+  myName = "Akk"
+  MYNAME = "Akk"
+  myname3 = "Akk"
+  print(my_name)
+  print(myname)
+  print(_my_name)
+  print(myName)
+  print(MYNAME)
+  print(myname3)
+  w, x, y = "Apple", "Mango", "Melon"
+  print(w)
+  print(x)
+  print(y)
+  a= k= m = "Football"
+  print(a)
+  print(k)
+  print(m)
+  talents = ["Singing", "Dancing", "Acting"]
+  u, v, q =talents
+  print(u)
+  print(v)
+  print(q)
+  p = "cool"
+  print("Programming is " + p)
+  k = 21
+  w = 19
+  print(k + w)
